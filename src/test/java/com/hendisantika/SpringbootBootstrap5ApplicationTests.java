85de7151214dda66354ec5771f2b7c44a21d30f7
Hendi Santika <hendisantika@gmail.com>
2021-09-05 17:09:13 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/SpringbootBootstrap5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBootstrap5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
